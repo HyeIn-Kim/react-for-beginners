d470c1ebcf89c53a7094b1fa66ffb67c71aa0c2f
HyeIn-Kim <fallingstars2749@gmail.com>
2022-01-14 14:01:17 +0900

    init: Initialize Project

---
+++ src/routes/Detail.js
@@ -0,0 +1,24 @@
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const getMovie = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+  }, [id]);
+  useEffect(() => {
+    getMovie();
+  }, [getMovie]);
+  return (
+    <div>
+      <h1>Detail</h1>
+      <h2>{movie.title}</h2>
+    </div>
+  );
+}
+
+export default Detail;
